fcffd7b22da732e50302e7fe4c7204ffe2628568
Marcus Meira <marcusvmeirap@gmail.com>
2024-04-12 12:41:17 -0300

    created alternate gather data function

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def gather_data_alternate():
+    n1 = int(1265)
+    n2 = int(300)
+    op = '/'
+
+    return n1, n2, op
+
+
 def gather_data():
     n1 = int(input("First Value:"))
     n2 = int(input("Second Value:"))
@@ -7,7 +15,7 @@ def gather_data():
 
 
 def main():
-    n1, n2, op = gather_data()
+    n1, n2, op = gather_data_alternate()
 
     print(eval(n1+op+n2))
     return None
